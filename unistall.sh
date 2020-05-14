070da5a05bcd13dda61a7d21fd10d1257d36e511
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-15 00:29:16 +0200

    renamed

--- unistall.sh
+++
@@ -1,8 +0,0 @@
-sudo rm /usr/bin/hilvertCurve &&
-sudo rm /usr/share/applications/hilvertCurve.desktop &&
-sudo rm /usr/share/icons/hilvertCurve.png &&
-echo "hilvertCurve removed successfully" ||
-echo "
-~~~~~~~~    ERROR AT UNINSTALLATION   ~~~~~~~~
-    Not posible to complete the unistallation
-"
