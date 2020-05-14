432d106f64d472553ebe68e32c726b503b627f41
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-14 20:23:06 +0200

    basic logic added. Algo needed :S

--- hilvertCurve.py
+++ hilvertCurve.py
@@ -25,28 +25,47 @@ class color():
         list = [colorSet[colorTitle] for colorTitle in [colorTitle for colorTitle in colorSet]]
         return list[np.random.randint(len(list))]
 
+def indexToCoord(x, y, *center):
+    centerc = 0 if not center else 0.5 
+    return (int((x + centerc) * sizeWidth + marginFrame), int((y + centerc) * sizeWidth + marginFrame))
+
 def getCubeCoord(x, y, *smaller): # Returns the same coordinates with the margin frame
-    smallerC = 0 if not smaller else round(sizeWidthX / 14)
+    
     rawCoord = [ # get coord of the cornes of the square
-        (x * sizeWidthX + smallerC, y * sizeWidthY + smallerC),
-        ((x + 1) * sizeWidthX - smallerC, y * sizeWidthY + smallerC),
-        ((x + 1) * sizeWidthX - smallerC, (y + 1) * sizeWidthY - smallerC),
-        (x * sizeWidthX + smallerC, (y + 1) * sizeWidthY - smallerC)
+        indexToCoord(x, y),
+        indexToCoord(x + 1, y),
+        indexToCoord(x + 1, y + 1),
+        indexToCoord(x, y + 1)
     ]
-    return [tuple(map(lambda i: i + marginFrame, tu)) for tu in rawCoord]
+    return rawCoord
+    # return [tuple(map(lambda i: i + marginFrame, tu)) for tu in rawCoord]
 
 def updateScreen():
+    
     screen.fill(COLOR.BG) # Clean screen
     for x in range(pow(2, level)): # for each spot in the grid
-        for y in range(pow(2, level)):
-            # Draw the grid
+        for y in range(pow(2, level)): # Draw the grid
             pygame.draw.polygon(screen, COLOR.GRID, getCubeCoord(x, y), 1) # print the grid
 
     # Score and level:
-    # screen.blit(scoreLabel, (17.25 * sizeWidthX, 5 * sizeWidthY))
-    # screen.blit(font.render(str(score), False, COLOR.WHITE), ((18.5 - (len(str(score))-1)/4) * sizeWidthX, 6 * sizeWidthY))
+    # screen.blit(scoreLabel, (17.25 * sizeWidth, 5 * sizeWidth))
+    # screen.blit(font.render(str(score), False, COLOR.WHITE), ((18.5 - (len(str(score))-1)/4) * sizeWidth, 6 * sizeWidth))
+    hilvertCurve(level)
     pygame.display.flip() # Update the screen
 
+def hilvertCurve(level):
+    # What I know:
+    # There are pow(2, level) cubes
+
+    # if level == 1:
+
+    for x in range(pow(2, level)):
+        for y in range(pow(2, level)):
+            pygame.draw.circle(screen, COLOR.ORANGE, indexToCoord(x, y, True), 5)
+
+
+    # pygame.draw.circle(screen, COLOR.ORANGE, indexToCoord(pow(2, level) - 1, pow(2, level) - 1), 5)
+    # pygame.draw.circle(screen, COLOR.ORANGE, indexToCoord(0, 0), 5)
 
 # -------------     CLASSES      -------------
 
@@ -56,7 +75,6 @@ pygame.init() # Init pygame
 pygame.display.set_caption("Jkutkut's Tetris") # Set the title of the game
 
 # CONSTANTS:
-level = 1
 width = 500 
 height = width
 extraWidth = 0 # Some space to display the score, level, next piece...
@@ -68,7 +86,8 @@ levelLabel = font.render('Level:', False, COLOR.WHITE)
 
 
 # Variables:
-score, level = 0, 0
+level = 1
+sizeWidth = width / pow(2, level)
 screen = pygame.display.set_mode((width + extraWidth + 2 * marginFrame, height + 2 * marginFrame)) # Set the size of the window
 
 lastGameTick = time.process_time() # store when we started (updates the screen)
@@ -80,7 +99,7 @@ gameRunning = True # If false, the game stops
 while gameRunning:
     # if time.process_time() - lastGameTick > 0.25 - level/100: # Update the screen but game ticks every some period 
     #     lastGameTick = time.process_time() # Update current game tick
-    
+    sizeWidth = width / pow(2, level)
     for event in pygame.event.get(): # for each event
         if event.type == pygame.QUIT: # if quit btn pressed
             gameRunning = False # no longer running game
